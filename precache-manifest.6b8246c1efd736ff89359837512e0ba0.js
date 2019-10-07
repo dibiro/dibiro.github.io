3f83efdfa25aef2d58717d0b957953527242b40e
Leopoldo Ramon Montesinos Moreno <leopoldodibiro@gmail.com>
2019-10-07 11:02:51 -0500

    update

---
+++ precache-manifest.6b8246c1efd736ff89359837512e0ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3fb46a727339bb79cdd59bea5bca83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd22f0a8106b26f5409",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cf7014d95f86fb84a062",
+    "url": "/static/js/2.602e9a42.chunk.js"
+  },
+  {
+    "revision": "7bd22f0a8106b26f5409",
+    "url": "/static/js/main.9f49dd24.chunk.js"
+  },
+  {
+    "revision": "1d591f7cdd13a3e7289d",
+    "url": "/static/js/runtime-main.53a0550f.js"
+  },
+  {
+    "revision": "b68b9f233a4864eccb6e968d6f638d38",
+    "url": "/static/media/22.b68b9f23.jpg"
+  }
+]);
